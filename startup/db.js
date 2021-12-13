ae4c9904d6b4be56c3f97cf01c6f85e0f030f4d5
Nay Thit Htoo <thithtoo2580@gmail.com>
2021-12-13 21:36:41 +0630

    rechange connetion string

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,17 @@
-const winston = require('winston');
 const mongoose = require('mongoose');
 
+const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+
 module.exports = function () {
-    mongoose.connect(process.env.DB)
-        .then(() => winston.info('Connected to MongoDB...'));
+    mongoose.connect(process.env.DB, connectionParams)
+        .then(() => {
+            console.log('Connected to database ')
+        })
+        .catch((err) => {
+            console.error(`Error connecting to the database. \n${err}`);
+        })
 }
