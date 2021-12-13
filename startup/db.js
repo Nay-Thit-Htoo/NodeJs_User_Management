dbdb0878504fbb097bce7a71f899e5c1cbd44db6
Nay Thit Htoo <thithtoo2580@gmail.com>
2021-12-13 21:08:22 +0630

    rechange connection string url

--- startup/db.js
+++ startup/db.js
@@ -2,6 +2,6 @@ const winston = require('winston');
 const mongoose = require('mongoose');
 
 module.exports = function () {
-    mongoose.connect('mongodb://localhost/UserRegistration')
+    mongoose.connect(process.env.DB)
         .then(() => winston.info('Connected to MongoDB...'));
 }
